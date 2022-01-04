ed20312f8a90c34ffd3235fe930a2fd8f678b3b9
aimad bachar <aimadbacharpro@outlook.fr>
2022-01-04 14:28:52 +0100

    change picture when hover in li color

--- js/main.js
+++ js/main.js
@@ -2,35 +2,26 @@ const app = Vue.createApp({
     data() {
         return {
             productName : 'Iphone 13',
-            productDescription : 'Very elegant phone',
+            productDescription : 'Téléphone très élégant',
             productPrice : 1000,
             productImage : '../assets/images/iphone-13-blue.png',
+            colors: ['red', 'blue', 'midnight'],
             quantity: 30,
             inStock: true,
             cartCount: 0,
-            features: [
-                {
-                    name: 'Dimensions',
-                    value: '4.7 x 1.8 x 7.9 cm'
-                },
-                {
-                    name: 'Weight',
-                    value: '143 g'
-                },
-                {
-                    name: 'Display',
-                    value: '5.8 inch'
-                }
-            ],
+            features: ['5G speed', 'Edge-to-edge OLED display', 'Ceramic Shield']
         }
         
     },
     methods: {
+        // add a product to cart
         addToCart() {
             this.cartCount += 1;
         },
-        removeFromCart() {
-            this.cartCount -= 1;
+        // update the product image when the color is changed with hover
+        updateProduct(color) {
+            hover = false;
+            this.productImage = '../assets/images/iphone-13-' + color + '.png';
         }
     }
 });
